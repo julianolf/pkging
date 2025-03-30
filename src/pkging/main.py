63fab0ae0222fe3d6e60946267ceb0aadba6b03e
Juliano Fernandes <julianofernandes@gmail.com>
2025-03-30 18:09:18 -0300

    Update argument parser function
    - Changed some of the argument names to provide better descriptions.
    - Added some arguments.
    - Created a data structure to build from the parsed arguments. This provides better support for LSP tools and improves readability.

--- src/pkging/main.py
+++ src/pkging/main.py
@@ -1,15 +1,73 @@
 import argparse
+import dataclasses
+import pathlib
+import typing
 
 from pkging import __appname__, __version__
 
+CURRENT_DIR = pathlib.Path(".").resolve()
+BUILD_DIR = CURRENT_DIR / "build"
+DEFAULT_OUTPUT = "obj"
+DEFAULT_INTERPRETER = "/usr/bin/env python3"
 
-def parse_args() -> argparse.Namespace:
+
+@dataclasses.dataclass
+class Args:
+    source: pathlib.Path
+    target: pathlib.Path
+    output: str = DEFAULT_OUTPUT
+    interpreter: str = DEFAULT_INTERPRETER
+    main: typing.Optional[str] = None
+
+
+def parse_args() -> Args:
     parser = argparse.ArgumentParser(
         prog=__appname__,
         description="Build a single executable file of your Python program.",
     )
-    parser.add_argument("--version", action="version", version=f"{__appname__} {__version__}")
-    return parser.parse_args()
+    parser.add_argument(
+        "--source",
+        type=pathlib.Path,
+        default=CURRENT_DIR,
+        help="source directory (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--target",
+        type=pathlib.Path,
+        default=BUILD_DIR,
+        help="target directory (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        default=DEFAULT_OUTPUT,
+        help="the name for the executable (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--interpreter",
+        type=str,
+        default=DEFAULT_INTERPRETER,
+        help="the Python interpreter with which the archive will be executed (default: %(default)s)",  # noqa: E501
+    )
+    parser.add_argument(
+        "--main",
+        type=str,
+        help="the name of a callable which will be used as the main program",
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"{__appname__} {__version__}",
+    )
+    args = parser.parse_args()
+
+    return Args(
+        source=args.source.resolve(),
+        target=args.target.resolve(),
+        output=args.output,
+        interpreter=args.interpreter,
+        main=args.main,
+    )
 
 
 def main() -> None:
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,3 @@
-import argparse
 import io
 import sys
 import unittest
@@ -11,7 +10,12 @@ class TestParseArgs(unittest.TestCase):
     @mock.patch.object(sys, "argv", ["pkging"])
     def test_parse_args(self) -> None:
         args = main.parse_args()
-        self.assertIsInstance(args, argparse.Namespace)
+        self.assertIsInstance(args, main.Args)
+        self.assertEqual(args.source, main.CURRENT_DIR)
+        self.assertEqual(args.target, main.BUILD_DIR)
+        self.assertEqual(args.output, main.DEFAULT_OUTPUT)
+        self.assertEqual(args.interpreter, main.DEFAULT_INTERPRETER)
+        self.assertIsNone(args.main)
 
     @mock.patch.multiple(sys, argv=["pkging", "--version"], stdout=io.StringIO())
     def test_parse_args_show_version_and_exit(self) -> None:
